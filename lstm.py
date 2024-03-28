eafaf26a2e9e0d320141103f52046d35874323c6
maximpopov11 <icecreamguy0@gmail.com>
2024-03-28 11:59:32 -0500

    Implement LSTM

---
+++ lstm.py
@@ -0,0 +1,39 @@
+import torch.nn as nn
+from torch.optim import Adam
+
+import lightning
+
+SEED = 0
+INPUT_SIZE = 33 * 2
+HIDDEN_SIZE = 5
+LEARNING_RATE = 0.1
+
+
+class LightningLSTM(lightning.LightningModule):
+    def __init__(self):
+        super().__init__()
+        lightning.seed_everything(seed=SEED)
+        self.lstm = nn.LSTM(input_size=INPUT_SIZE, hidden_size=HIDDEN_SIZE)
+
+    def forward(self, input):
+        input_trans = input.view(len(input), 1)
+        lstm_out, temp = self.lstm(input_trans)
+        prediction = lstm_out[-1]
+        return prediction
+
+    def configure_optimizers(self):
+        return Adam(self.parameters(), lr=LEARNING_RATE)
+
+    def training_step(self, batch, batch_idx):
+        input_i, label_i = batch
+        output_i = self.forward(input_i[0])
+        loss = (output_i - label_i) ** 2
+
+        self.log("train_loss", loss)
+
+        if label_i == 0:
+            self.log("out_0", output_i)
+        else:
+            self.log("out_1", output_i)
+
+        return loss
